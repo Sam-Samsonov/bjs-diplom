af3a2d2c02c5b2c7407e611fcf115bae43cce8a6
Sergei Martushev <martuschev.sam@yandex.ru>
2021-12-20 19:04:09 +0300

    diplom second commit

--- public/homePage.js
+++ public/homePage.js
@@ -1,7 +1,89 @@
 const logoutButton = new LogoutButton;
-logoutButton.action = (logout) => {
-  if (ApiConnector.logout(logout => console.log(logout))) {
-    location.reload();
-    return logout;
+logoutButton.action = () => {
+  ApiConnector.logout(data => {
+    if (data.success === true) {
+      setTimeout(() => document.location.reload(), 500);
+    }
+  })
+}
+
+ApiConnector.current(data => {
+  if (data.success === true) {
+    ProfileWidget.showProfile(data.data)
   }
+})
+
+const ratesBoard = new RatesBoard;
+setInterval(() => {
+ApiConnector.getStocks(data => {
+  ratesBoard.clearTable();
+  ratesBoard.fillTable(data.data);
+})
+}, 1000)
+
+const moneyManager = new MoneyManager;
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (data) => {
+    if (data.success === true) {
+      ProfileWidget.showProfile(data.data);
+      moneyManager.setMessage(true, 'Пополнение успешно выполнено)');
+    } else {
+      moneyManager.setMessage(false, data.error);
+    }
+  })
+}
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (data) => {
+    if (data.success === true) {
+      ProfileWidget.showProfile(data.data);
+      moneyManager.setMessage(true, 'Конвертирование успешно выполнено)');
+    } else {
+      moneyManager.setMessage(false, data.error);
+    }
+  })
+}
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (data) => {
+    if (data.success === true) {
+      ProfileWidget.showProfile(data.data);
+      moneyManager.setMessage(true, 'Трансфер успешно выполнен)');
+    } else {
+      moneyManager.setMessage(false, data.error);
+    }
+  })
+}
+
+const favoritesWidget = new FavoritesWidget;
+ApiConnector.getFavorites(data => {
+  if (data.success === true) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(data.data);
+    moneyManager.updateUsersList(data.data);
+  }
+})
+
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (data) => {
+    if (data.success === true) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(data.data);
+      moneyManager.updateUsersList(data.data);
+    } else {
+      favoritesWidget.setMessage(false, data.error);
+    }
+  })
+}
+
+favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (data) => {
+    if (data.success === true) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(data.data);
+      moneyManager.updateUsersList(data.data);
+    } else {
+      favoritesWidget.setMessage(false, data.error);
+    }
+  })
 }
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,7 +1,21 @@
 const userForm = new UserForm;
+
 userForm.loginFormCallback = (data) => {
-    //console.log(data);
-  if (ApiConnector.login(data,(data) => console.log(data))) {
-    location.reload();
-  }
+  ApiConnector.login(data,(data) => {
+    if (data.success === false) {
+      userForm.setLoginErrorMessage(data.error);
+    } else {
+      setTimeout(() => document.location.reload(), 500);
+    }
+  })
+}
+
+userForm.registerFormCallback = (data) => {
+  ApiConnector.register(data,(data) => {
+    if (data.success === false) {
+      userForm.setRegisterErrorMessage(data.error);
+    } else {
+      setTimeout(() => document.location.reload(), 500);
+    }
+  })
 }
